7e457bf423a197f8f04642e7dfeae7134d807624
MRAO0212 <mayureshvrao@gmail.com>
2021-03-26 08:52:33 -0700

    Bronze Challenge 2

---
+++ NRCH1.playground/Pages/NRCH2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 4455
+let townName: String = "Nagpur"
+let elevation : Float = 2200
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights with elevation \(elevation)"
+print(townDescription)
+//: [Next](@next)
