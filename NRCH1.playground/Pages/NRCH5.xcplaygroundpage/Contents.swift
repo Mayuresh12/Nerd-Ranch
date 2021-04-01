9612172bb2f9f61db72bc51180d0a64906d04c95
MRAO0212 <mayureshvrao@gmail.com>
2021-04-01 00:21:51 -0700

    Bronze, Silver Challenge 5

--- NRCH1.playground/Pages/NRCH5.xcplaygroundpage/Contents.swift
+++ NRCH1.playground/Pages/NRCH5.xcplaygroundpage/Contents.swift
@@ -28,4 +28,30 @@ case 500...505:
 case let code:
     errorString = "\(code) is not a known error code"
 }
+
+// Bronze Challenge
+
+let point = (x: 1, y : 4)
+switch point {
+case let q1 where (point.x > 0) && (point.y > 0):
+    print("\(q1) is in Quadrant 1")
+case let q2 where (point.x < 0) && (point.y > 0):
+    print("\(q2) is in Quadrant 2")
+case let q3 where (point.x < 0) && (point.y < 0):
+    print("\(q3) is in Quadrant 3")
+case let q4 where (point.x > 0) && (point.y < 0):
+    print("\(q4) is in Quadrant 4")
+case (_,0):
+    print("\(point) sits on X-axis.")
+case (0,_):
+    print("\(point) sits on Y-axis.")
+default:
+    print("Case not covered")
+}
+
+// Silver Challenge
+let age = 25
+if case 18...35 = age, age >= 25 , age < 30, age > 18 {
+    print("In cool demographic and can rent the car.")
+}
 //: [Next](@next)
